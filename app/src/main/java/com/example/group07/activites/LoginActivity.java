bce84d8207983daec2c0ca0fcc5b6859d32c5897
Kira <56455045+kiracherie@users.noreply.github.com>
2020-11-20 16:22:43 -0800

    add needed imports to read from R

--- app/src/main/java/com/example/group07/activites/LoginActivity.java
+++ app/src/main/java/com/example/group07/activites/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.group07.activites;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.group07.R;
 
 public class LoginActivity extends AppCompatActivity {
 
